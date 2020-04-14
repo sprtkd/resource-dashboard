b4102c5d4bf310cfe0052bfc1e515b46f5dbcc8a
Suprotik Dey <supdey1@gmail.com>
2020-04-14 22:13:32 +0530

    Added routes and generated dummy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { ErrorComponent } from './common/error/error.component';
+import { LoginComponent } from './landing/login/login.component';
+import { RegisterComponent } from './landing/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ViewLogsComponent } from './view-logs/view-logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    ErrorComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ViewLogsComponent
   ],
   imports: [
     BrowserModule,
