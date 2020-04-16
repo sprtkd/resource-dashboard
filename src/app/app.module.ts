5727493ed5dcda4e3165a2c24760ea6bf59ed6cc
Suprotik Dey <supdey1@gmail.com>
2020-04-16 18:28:55 +0530

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,13 @@ import { RegisterComponent } from './landing/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ViewLogsComponent } from './view-logs/view-logs.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialNavbarComponent } from './common/material-navbar/material-navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -23,12 +30,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LoginComponent,
     RegisterComponent,
     DashboardComponent,
-    ViewLogsComponent
+    ViewLogsComponent,
+    MaterialNavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
