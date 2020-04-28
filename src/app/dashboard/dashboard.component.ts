468205ebf44037bc292e7bd6929e61f4bf3fbee2
Suprotik Dey <supdey1@gmail.com>
2020-04-28 17:33:47 +0530

    Refacting

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonsService } from '../services/commons.service';
+import { UserUiLogin } from '../models/ui/user.ui.login';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  profileType: String;
-  constructor() { }
+  profileType: UserUiLogin;
+  constructor(private commonsService: CommonsService) { }
 
   ngOnInit(): void {
-    this.profileType = "supportRep";
+    this.profileType = this.commonsService.getLoggedIn();
   }
 
 }
