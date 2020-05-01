01fad141dd4008cf16f923d2a1421a4e971bc86e
rednani7 <riniednani20@gmail.com>
2020-05-01 15:26:44 +0530

    Added Registration form with basic validations

--- src/app/landing/register/register.component.ts
+++ src/app/landing/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder , FormGroup ,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+   userForm: FormGroup;
+   message: String;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      UserName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      Password: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      Contact: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]]
+    });
+  }
+
+  onSubmit(){
+    if(this.userForm.valid){
+      this.message ='User Registraction Form is Valid!! Submitted to Reviewer';
+    } else {
+      this.message='User form is not valid!!';
+    }
   }
 
 }
